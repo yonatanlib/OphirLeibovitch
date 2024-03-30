5911441cecd8cfdb21c40f65f42f82e039573aba
Yonatan Leibovitch <yonyon.bsr@gmail.com>
2024-03-30 12:37:52 +0300

    moved to typescrit

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,27 @@
   <div id="app">
     <header-logo />
     <div class="pages">
-    <side-menu/>
-    <!-- <about-page /> -->
-    <contact-page/>
+      <side-menu />
+      <about-page />
+      <!-- <contact-page /> -->
     </div>
   </div>
 </template>
 
-<script>
-import ContactPage from './components/contactPage.vue';
+<script lang="ts">
+import Vue from "vue";
+import aboutPage from "./components/aboutPage.vue";
 import headerLogo from "./components/headerLogo.vue";
-// import AboutPage from "./components/aboutPage.vue";
-import SideMenu from './components/sideMenu.vue';
-export default {
+import sideMenu from "./components/sideMenu.vue";
+export default Vue.extend({
   name: "App",
-  components: { headerLogo, SideMenu, ContactPage },
-};
+  components: {
+    headerLogo,
+    sideMenu,
+    // contactPage,
+    aboutPage,
+  },
+});
 </script>
 
 <style>
@@ -25,14 +30,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-  margin: 0 5rem;
-}
-body {
-  margin: 0;
-}
-.pages {
-  display: flex;
-  margin-top:1rem;
+  margin-top: 60px;
 }
 </style>
